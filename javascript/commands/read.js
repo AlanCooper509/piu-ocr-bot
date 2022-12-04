fa760a3db9a14ebd855165ac4c669e5d100b9a2c
alancooper509 <alancooper509@hotmail.com>
2022-12-04 02:38:00 -0500

    added button interactions and SCORES modal as first step for creating a feature to edit the scores

--- javascript/commands/read.js
+++ javascript/commands/read.js
@@ -14,26 +14,51 @@ module.exports = (message) => {
     }
 
     let messageAttachment = message.attachments.first().url;
-    let embed = new Discord.EmbedBuilder();
-    embed.setAuthor({
-        name: message.author.tag,
-        iconURL: message.author.avatarURL()
-    });
-    embed.setImage(messageAttachment);
-    embed.setColor(14680086);
-    embed.setDescription('Some description here');
-    embed.addFields(
-        { name: 'Regular field title', value: 'Some value here' },
-        { name: '\u200B', value: '\u200B' },
-        { name: 'Inline field title', value: 'Some value here', inline: true },
-        { name: 'Inline field title', value: 'Some value here', inline: true },
-    );
     console.log(`OCR processing: ${messageAttachment}`);
     const pythonProcess = spawn('python', ["../python/readscores.py", messageAttachment]);
     pythonProcess.stdout.on('data', (data) => {
         console.log(`OCR success: ${messageAttachment}`);
-        embed.addFields({ name: 'readscores.py', value: data.toString() });
-        message.channel.send({ embeds: [embed] });
+
+        let embed = new Discord.EmbedBuilder();
+        embed.setAuthor({
+            name: message.author.tag,
+            iconURL: message.author.avatarURL()
+        });
+        embed.setImage(messageAttachment);
+        embed.setColor(14680086);
+        embed.setDescription('Some description here');
+        /*
+        embed.addFields(
+            { name: 'Regular field title', value: 'Some value here' },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+        );
+        */
+        embed.addFields(
+            { name: 'readscores.py', value: data.toString() }
+        );
+
+        // Button for triggering edits
+        const row = new Discord.ActionRowBuilder()
+            .addComponents(
+                new Discord.ButtonBuilder()
+                    .setCustomId(c.DEV_MODAL_EDIT_SCORES_BUTTON)
+                    .setLabel('⚖️ SCORES')
+                    .setStyle(Discord.ButtonStyle.Success),
+                // TODO: not yet implemented in interactionCreate.js
+                new Discord.ButtonBuilder()
+                    .setCustomId("NOT YET IMPLEMENTED1")
+                    .setLabel('🔗 COMBO')
+                    .setStyle(Discord.ButtonStyle.Secondary),
+                // TODO: not yet implemented in interactionCreate.js
+                new Discord.ButtonBuilder()
+                    .setCustomId("NOT YET IMPLEMENTED2")
+                    .setLabel('💯 TOTAL')
+                    .setStyle(Discord.ButtonStyle.Primary)
+            );
+
+        message.channel.send({ embeds: [embed], components: [row] });
         message.delete();
     });
 };
--- javascript/resources/constants.js
+++ javascript/resources/constants.js
@@ -1,1 +1,4 @@
 exports.COMMAND_READ = "score";
+
+exports.DEV_MODAL_EDIT_SCORES_BUTTON = "editScoreModalButton";
+exports.DEV_MODAL_EDIT_SCORES_ID = "editScoreModal";
