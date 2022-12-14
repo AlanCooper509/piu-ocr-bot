602b6aa21acb9b7b1ea3d9102d83bc8e86a06a5d
alancooper509 <alancooper509@hotmail.com>
2022-12-14 13:38:09 -0800

    saving modified status in DB for embed tweaking later

--- javascript/database-reset.js
+++ javascript/database-reset.js
@@ -15,33 +15,45 @@ db.serialize(() => {
     // first operation (run create table statement)
     let sql = 
         `CREATE TABLE IF NOT EXISTS ${process.env.DB_SCORES_TABLE} (
-        id            INTEGER PRIMARY KEY,
-        discord_id    INTEGER NOT NULL,
-        server_id     NUMERIC NOT NULL,
-        game_id       TEXT,
-        image_url     TEXT,
-        chart_name    TEXT,
-        chart_type    TEXT,
-        chart_diff    INTEGER CHECK (chart_diff >= -1),
-        grade         TEXT,
-        break_on      INTEGER CHECK (break_on == 0 OR 
-                                     break_on == 1) 
-                              DEFAULT (0),
-        perfects      INTEGER CHECK (perfects >= 0),
-        greats        INTEGER CHECK (greats >= 0),
-        goods         INTEGER CHECK (goods >= 0),
-        bads          INTEGER CHECK (bads >= 0),
-        misses        INTEGER CHECK (misses >= 0),
-        max_combo     INTEGER CHECK (max_combo >= 0),
-        total_score   INTEGER CHECK (total_score >= 0),
-        time_uploaded TEXT,
-        time_modified TEXT);`;
+            id               INTEGER PRIMARY KEY,
+            discord_id       INTEGER NOT NULL,
+            server_id        NUMERIC NOT NULL,
+            game_id          TEXT,
+            image_url        TEXT    NOT NULL,
+            chart_name       TEXT,
+            chart_type       TEXT,
+            chart_diff       INTEGER CHECK (chart_diff >= -1),
+            grade            TEXT,
+            break_on         INTEGER CHECK (break_on == 0 OR 
+                                            break_on == 1) 
+                                     DEFAULT (0),
+            details_modified TEXT    CHECK (details_modified == 0 OR 
+                                            details_modified == 1) 
+                                     DEFAULT (0),
+            perfects         INTEGER CHECK (perfects >= 0),
+            greats           INTEGER CHECK (greats >= 0),
+            goods            INTEGER CHECK (goods >= 0),
+            bads             INTEGER CHECK (bads >= 0),
+            misses           INTEGER CHECK (misses >= 0),
+            scores_modified  INTEGER CHECK (scores_modified == 0 OR 
+                                            scores_modified == 1) 
+                                     DEFAULT (0),
+            max_combo        INTEGER CHECK (max_combo >= 0),
+            combo_modified   INTEGER CHECK (combo_modified == 0 OR 
+                                            combo_modified == 1) 
+                                     DEFAULT (0),
+            total_score      INTEGER CHECK (total_score >= 0),
+            total_modified   INTEGER DEFAULT (0) 
+                                     CHECK (total_modified == 0 OR 
+                                            total_modified == 1),
+            time_uploaded    TEXT,
+            time_modified    TEXT);`;
     db.run(sql, (err) => {
         if (err) {
             console.log(err);
             throw err;
         } else {
-            console.log("COMPLETE");
+            console.log("CREATE TABLE COMPLETE");
         }
     });
 });
