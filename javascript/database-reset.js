ac153a1d6afa81c48832495fa73e1a4180664cd9
alancooper509 <alancooper509@hotmail.com>
2022-12-13 17:18:00 -0800

    adding a simple db setup script

--- javascript/database-reset.js
+++ javascript/database-reset.js
@@ -12,8 +12,30 @@ const db = new sqlite3.Database(process.env.DB_NAME, (err) => {
 });
 
 db.serialize(() => {
-    // 1rst operation (run create table statement)
-    let sql = `CREATE TABLE IF NOT EXISTS users(email text, password text)`;
+    // first operation (run create table statement)
+    let sql = 
+        `CREATE TABLE PlayerRecords (
+        id            INTEGER PRIMARY KEY,
+        discord_id    INTEGER NOT NULL,
+        server_id     NUMERIC NOT NULL,
+        game_id       TEXT,
+        image_url     TEXT,
+        chart_name    TEXT,
+        chart_type    TEXT,
+        chart_diff    INTEGER CHECK (chart_diff > 0),
+        grade         TEXT,
+        break_on      INTEGER CHECK (break_on == 0 OR 
+                                     break_on == 1) 
+                              DEFAULT (0),
+        perfects      INTEGER CHECK (perfects >= 0),
+        greats        INTEGER CHECK (greats >= 0),
+        goods         INTEGER CHECK (goods >= 0),
+        bads          INTEGER CHECK (bads >= 0),
+        misses        INTEGER CHECK (misses >= 0),
+        max_combo     INTEGER CHECK (max_combo >= 0),
+        total_score   INTEGER CHECK (total_score >= 0),
+        time_uploaded TEXT,
+        time_modified TEXT);`;
     db.run(sql, (err) => {
         if (err) {
             console.log(err);
