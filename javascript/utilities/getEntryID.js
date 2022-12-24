520edee7b876522d6e8116a4c270abdeb5e4ae36
alancooper509 <alancooper509@hotmail.com>
2022-12-23 19:37:26 -0600

    comments for clarity on method scope and objective

--- javascript/utilities/getEntryID.js
+++ javascript/utilities/getEntryID.js
@@ -2,6 +2,15 @@
 const c = require("../resources/constants.js");
 
 module.exports = (interaction) => {
+    /**
+     * Expected Input:
+     *     messageEmbed.Fields[target_index] {
+     *         name: "c.EMBED_FIELD_RECORD_ID",
+     *         value: "<target record ID>", // no additional formatting on the value
+     *         <...>
+     *     }
+     * }
+     **/
     const messageEmbed = interaction.message.embeds[0];
     const scoresField = messageEmbed.fields.find(e => e.name.includes(c.EMBED_FIELD_RECORD_ID));
     return scoresField.value;
